67e00cc93be2dcfaac086f1506ddb28d64326eaa
Vetle Ørstavik Hollund <vetleh15@uia.no>
2020-12-18 11:33:33 +0100

    day 17 and 18 start

--- Helpers/GetInput.py
+++ Helpers/GetInput.py
@@ -4,6 +4,13 @@ def get_input_split_lines(day):
         return lines
 
 
+def get_file_split_lines(file):
+    with open(file, 'r') as input:
+        lines = "".join(input.readlines()).split("\n")
+        return lines
+
+
+
 def get_input_tuple(day):
     with open(f'Day{day}\input.txt', 'r') as input:
         lines = "".join(input.readlines()).split("\n")
